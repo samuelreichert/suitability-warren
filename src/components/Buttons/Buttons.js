1800358fee64a1d90a5de2ab3391c656d838d081
Samuel Reichert <samuelreichertt@gmail.com>
2020-10-04 23:03:46 -0300

    Create Buttons commponent to load dynamic buttons

---
+++ src/components/Buttons/Buttons.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Button, Div } from 'atomize';
+
+const Buttons = ({ buttons, answers, setAnswers, id }) => {
+  const onClickButton = (value) => {
+    setAnswers({
+      ...answers,
+      [id]: value
+    })
+  };
+
+  return (
+    <Div d='flex' justify='space-evenly' w='100%'>
+      {
+        buttons.map(({ label: { title }, value }, i) =>
+          <Button
+            key={i}
+            bg='info300'
+            hoverBg='info400'
+            onClick={() => onClickButton(value)}
+            rounded='circle'
+            shadow='2'
+            hoverShadow='3'
+            textColor='info700'
+          >
+            {title}
+          </Button>
+        )
+      }
+    </Div>
+  );
+};
+
+export default Buttons;
