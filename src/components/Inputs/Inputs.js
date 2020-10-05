640bed8fb656a2da253e48c59acec2871007dab3
Samuel Reichert <samuelreichertt@gmail.com>
2020-10-04 23:04:44 -0300

    Create Inputs component to show dynamic inputs

---
+++ src/components/Inputs/Inputs.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Input } from 'atomize';
+import { INPUT_TYPES } from './constants';
+
+const Inputs = ({ answers, id, inputs, onClickNext, setAnswers }) => {
+  const onChangeInput = (event) => {
+    setAnswers({
+      ...answers,
+      [id]: event.currentTarget.value
+    });
+  };
+
+  const onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      onClickNext(id);
+    }
+  };
+
+  return (
+    <>
+      {
+        inputs.map((input, i) => (
+          <Input
+            key={i}
+            name={input.mask}
+            type={INPUT_TYPES[input.type]}
+            rounded='circle'
+            onChange={onChangeInput}
+            onKeyPress={onKeyPress}
+          />
+        ))
+      }
+    </>
+  );
+};
+
+export default Inputs;
