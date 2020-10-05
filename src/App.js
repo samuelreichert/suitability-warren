cc48838e2276ac7e4e18e16ddaf36d413ee25b7a
Samuel Reichert <samuelreichertt@gmail.com>
2020-10-04 23:06:24 -0300

    Refactor App to use the newly created components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,77 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useMutation } from 'react-query';
-import { Button, Container, Div, Input, Text } from 'atomize';
-import { suitabilityConversation } from './AppQuery';
+import { Col, Container, Div, Row, Text } from 'atomize';
+
+import { suitabilityConversation, finishSuitability } from './AppQuery';
+
 import Loading from './components/Loading';
+import FinishSuitability from './components/FinishSuitability';
+import SuitabilityQuestion from './components/SuitabilityQuestion';
 
 const App = () => {
-  const [fetchSuitability, { data, error, isLoading, isError }] = useMutation(suitabilityConversation)
+  const [answers, setAnswers] = useState({});
+
+  const [fetchSuitability, { data, isLoading }] = useMutation(suitabilityConversation);
+  const suitabilityData = data?.data || {};
+
+  const [fetchProfile, profileResponse] = useMutation(finishSuitability);
+  const {
+    data: { data: { user } = {} } = {},
+    isSuccess: profileSuccess,
+    isLoading: profileLoading
+  } = profileResponse;
+
+  const trySuitability = useCallback((data) => {
+    try {
+      fetchSuitability(data);
+    } catch {
+      console.error('Algo deu errado na suitability request');
+    }
+  }, [fetchSuitability]);
 
   useEffect(() => {
-    fetchSuitability()
-  }, [fetchSuitability])
+    trySuitability();
+  }, [trySuitability]);
 
-  if (isLoading) {
+  if (isLoading || profileLoading) {
     return <Loading />;
   }
 
-  console.log(data)
-
   return (
-    <Container d='flex' align='center' justify='center' flexDir='column'>
+    <Container d='flex' align='center' justify='center' flexDir='column' bg='gray100' minW='100%' maxW='100%' w='100%'>
       <Text tag='h3' textSize='display1' m={{ y: '2rem' }} textColor='brand900'>
         Suitability Warren
       </Text>
 
-      <Div shadow='4' p='2rem' rounded='xl' d='flex' align='center' justify='space-between' flexDir='column'>
-        <Text>Pergunta aqui?</Text>
+      <Row align='center' justify='center' w='100%'>
+        <Col size={6} align='center' justify='center'>
+          <Div
+            align='center'
+            bg='white'
+            d='flex'
+            flexDir='column'
+            justify='space-between'
+            m={{ b: '3rem' }}
+            p='2rem'
+            rounded='xl'
+            shadow='4'
+            w='100%'
+            minW='100%'
+          >
+            {(profileSuccess && !profileLoading) && <FinishSuitability user={user} />}
 
-        <Input placeholder="Basic Input" m={{ t: '2rem' }} rounded='circle' w='100%'/>
-
-        <Button bg='brand800' w='100%' rounded='circle' m={{ t: '2rem' }}>Próxima</Button>
-      </Div>
+            {(!profileSuccess || profileLoading) &&
+              <SuitabilityQuestion
+                {...suitabilityData}
+                answers={answers}
+                setAnswers={setAnswers}
+                trySuitability={trySuitability}
+                fetchProfile={fetchProfile}
+              />
+            }
+          </Div>
+        </Col>
+      </Row>
     </Container>
   );
 };
--- src/AppQuery.js
+++ src/AppQuery.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 export const suitabilityConversation = (data = { context: 'suitability' }) => {
   const url = process.env.REACT_APP_API_URL;
-
   return axios.post(`${url}/conversation/message`, data)
+};
+
+export const finishSuitability = (data) => {
+  const url = process.env.REACT_APP_API_URL;
+  return axios.post(`${url}/suitability/finish`, data)
 };
