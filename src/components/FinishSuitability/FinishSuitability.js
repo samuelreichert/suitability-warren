0c93226c752c2d9b575bf21952b567b4dd96190e
Samuel Reichert <samuelreichertt@gmail.com>
2020-10-04 23:13:31 -0300

    Refactor FinishSuitability to have a better UX

--- src/components/FinishSuitability/FinishSuitability.js
+++ src/components/FinishSuitability/FinishSuitability.js
@@ -5,9 +5,11 @@ const FinishSuitability = ({ user }) => {
   return (
     <>
       <Text tag='h3' textSize='display1' m={{ y: '2rem' }} textColor='success700'>
+        Parabéns!
+      </Text>
+      <Text textSize='title' m={{ b: '2rem' }} textColor='success600'>
         {user.name} você tem o perfil {user.investmentProfile.riskToleranceProfile}!
       </Text>
-      <Text textSize='title' m={{ b: '2rem' }} textColor='success600'>Parabéns!</Text>
     </>
   );
 };
